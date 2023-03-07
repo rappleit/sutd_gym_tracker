6ef97f8600600517232daba6385585fb18045e50
Rachel <80181684+rappleit@users.noreply.github.com>
2023-03-07 16:51:13 +0800

    added pulling data from firebase db to frontend feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from './components/counter';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from 'firebase/database';
 
 function App() {
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const db = getDatabase();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter {...{db}} />
     </div>
   );
 }
---
+++ frontend/src/components/counter.js
@@ -0,0 +1,38 @@
+import { getDatabase, onValue, ref } from "firebase/database";
+import { useState, useEffect } from 'react';
+
+
+
+const Counter = ({
+    db
+}) => {
+    const currentDate = new Date();
+    const [numEntries, setNumEntries] = useState(0)
+    const [numExits, setNumExits] = useState(0)
+
+
+    const entryRef = ref(db, 'tracker/' + `${currentDate.getFullYear()}/` + `${currentDate.getMonth() + 1}/` + `${currentDate.getDate()}/` + 'entry')
+    useEffect(() => {
+        onValue(entryRef, (snapshot) => {
+            setNumEntries(snapshot.size); //pull number of entries from db
+        }, (error) => {
+            console.error(error);
+        });
+    
+        const exitRef = ref(db, 'tracker/' + `${currentDate.getFullYear()}/` + `${currentDate.getMonth() + 1}/` + `${currentDate.getDate()}/` + 'exit')
+        onValue(exitRef, (snapshot) => {
+            setNumExits(snapshot.size); //pull number of exits from db
+        }, (error) => {
+            console.error(error);
+        });
+    }, [])
+    
+    return (
+        <div className="counter">
+            <h1>Number of people in the gym:</h1>
+            <h1>{numEntries-numExits}</h1>
+        </div>
+    );
+}
+
+export default Counter;
